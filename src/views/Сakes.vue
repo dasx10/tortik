08f357a4977bad73f5f395fad8362155b0dc7a68
dasx10 <dasx10@gmail.com>
2020-10-17 15:58:46 +0300

    leha

--- src/views/Сakes.vue
+++ src/views/Сakes.vue
@@ -4,13 +4,27 @@
     <article>
       <h2>Наша продукция</h2>
       <section>
-        <div class="card" v-for="i in 10" :key="i">
+        <div class="card" v-for="(cace,i) in caces" :key="i" >
           <div class="card__content">
-            <h3>Название</h3>
-            <img src="/Cake.svg" alt="" srcset="">
+            <h3>{{cace}}</h3>
+            <img :src="`img/card ${i+1}.png`" alt="" srcset=""/>
           </div>
         </div>
       </section>
     </article>
   </main>
 </template>
+<script>
+export default {
+  data:()=>({
+    caces: [
+      "1",
+      "2",
+      "33",
+      "44",
+      "55",
+      "66",
+    ]
+  })
+}
+</script>
