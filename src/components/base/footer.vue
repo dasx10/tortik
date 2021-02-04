9b59f8b899db812af645ce8abb7be78aac0674f8
developer <dasx10@gmail.com>
2021-02-04 19:34:11 +0200

    ref

--- src/components/base/footer.vue
+++ src/components/base/footer.vue
@@ -1,27 +1,23 @@
 <template>
     <footer>
         <div>
-            <h4>мы в соц. сетях</h4>
-            <ul class="social__list">
-                <li class="social__item"><a href="" class="social__link"><img src="/instagram.svg" alt="" class="social__icon"></a></li>
-                <li class="social__item"><a href="" class="social__link"><img src="/facebook.svg" alt="" class="social__icon"></a></li>
-                <li class="social__item"><a href="" class="social__link"><img src="/telegram.svg" alt="" class="social__icon"></a></li>
+            <h4>Контакты</h4>
+            <ul class="soc__list">
+
             </ul>
         </div>
         <div>
-            <h4>мы в соц. сетях</h4>
-            <ul class="social__list">
-                <li class="social__item"><a href="" class="social__link"><img src="/instagram.svg" alt="" class="social__icon"></a></li>
-                <li class="social__item"><a href="" class="social__link"><img src="/facebook.svg" alt="" class="social__icon"></a></li>
-                <li class="social__item"><a href="" class="social__link"><img src="/telegram.svg" alt="" class="social__icon"></a></li>
+            <h4>Информация</h4>
+            <ul class="soc__list">
+
             </ul>
         </div>
         <div>
-            <h4>мы в соц. сетях</h4>
-            <ul class="social__list">
-                <li class="social__item"><a href="" class="social__link"><img src="/instagram.svg" alt="" class="social__icon"></a></li>
-                <li class="social__item"><a href="" class="social__link"><img src="/facebook.svg" alt="" class="social__icon"></a></li>
-                <li class="social__item"><a href="" class="social__link"><img src="/telegram.svg" alt="" class="social__icon"></a></li>
+            <h4>Мы в соц. сетях</h4>
+            <ul class="soc__list">
+                <li class="soc__item"><a href="" class="soc__link"><img src="/instagram.svg" alt="instagram" class="soc__icon"></a></li>
+                <li class="soc__item"><a href="" class="soc__link"><img src="/facebook.svg" alt="facebook" class="soc__icon"></a></li>
+                <li class="soc__item"><a href="" class="soc__link"><img src="/telegram.svg" alt="telegram" class="soc__icon"></a></li>
             </ul>
         </div>
     </footer>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <main>
     <h1>Торты на заказ</h1>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut excepturi culpa quo maxime vel, neque atque rem distinctio laboriosam inventore beatae at dolorem quae sit vero minus corrupti. Quibusdam, error!
   </main>
 </template>
